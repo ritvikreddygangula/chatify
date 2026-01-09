f95b7e9131be1409647576b09526af7b35229af7
ritvikreddygangula <ritvikreddygangula@gmail.com>
2026-01-09 11:14:42 -0700

    signup page implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/messages.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
-
+import cors from "cors";
 dotenv.config();
 const app = express();
 
@@ -16,6 +16,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); //re.body
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
